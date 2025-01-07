f75991c6a0961376a37021978656779b648baeff
badsha-ugv <badsha.ugv@gmail.com>
2025-01-07 09:42:32 +0600

    webhook updated

--- transactions/views.py
+++ transactions/views.py
@@ -21,48 +21,50 @@ def transaction_webhook(request):
         # print(data)
         extra_data = data['subscriber']['attributes']
         user_id = extra_data['db_user_id']['value']
+        credit_amount = extra_data['credit_amount']['value']
         print(user_id)
+
         # credit_amount = data.get('extra_data',{}).get('credit_amount')
 
-        # payment_id = data.get('event',{}).get('payment_id')
-        # transaction_id = data.get('event',{}).get('transaction_id')
-        # product_id = data.get('event',{}).get('product_id')
-        # payment_method = data.get('event',{}).get('payment_method','other')
-        # status = "SUCCESS" if data["event"]["type"] == "INITIAL_PURCHASE" else "FAILED"
+        payment_id = data.get('event',{}).get('payment_id')
+        transaction_id = data.get('event',{}).get('transaction_id')
+        product_id = data.get('event',{}).get('product_id')
+        payment_method = data.get('event',{}).get('payment_method','other')
+        status = "SUCCESS" if data["event"]["type"] == "INITIAL_PURCHASE" else "FAILED"
 
-        # type = data["event"]["type"]
-        # app_user_id = data["event"]["app_user_id"]
-        # purchase_date = data["event"]["purchase_date"]
-        # store = data["event"]["store"]
-        # price = data["event"]["price"]
-        # currency = data["event"]["currency"]
-        # region_code = data["event"]["region_code"]
+        type = data["event"]["type"]
+        app_user_id = data["event"]["app_user_id"]
+        purchase_date = data["event"]["purchase_date"]
+        store = data["event"]["store"]
+        price = data["event"]["price"]
+        currency = data["event"]["currency"]
+        region_code = data["event"]["region_code"]
 
-        # if user_id:
-        #     user = Users.objects.filter(user=user_id).first()
-        #     if not user:
-        #         return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        if user_id:
+            user = Users.objects.filter(user=user_id).first()
+            if not user:
+                return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        #     Transaction.objects.create(
-        #         user=user,
-        #         payment_id=payment_id,
-        #         transaction_id=transaction_id,
-        #         product_id=product_id,
-        #         payment_method=payment_method,
-        #         type=type,
-        #         app_user_id=app_user_id,
-        #         purchase_date=purchase_date,
-        #         store=store,
-        #         price=price,
-        #         currency=currency,
-        #         region_code=region_code,
-        #         credit_amount=credit_amount,
-        #     )
-        #     if status == "SUCCESS":
-        #         user_credit = CreditWallet.objects.get(user=user)
-        #         user_credit.balance += credit_amount
-        #         user_credit.save()
-            # return Response({'message': 'Transaction processed successfully'}, status=status.HTTP_200_OK)
+            Transaction.objects.create(
+                user=user,
+                payment_id=payment_id,
+                transaction_id=transaction_id,
+                product_id=product_id,
+                payment_method=payment_method,
+                type=type,
+                app_user_id=app_user_id,
+                purchase_date=purchase_date,
+                store=store,
+                price=price,
+                currency=currency,
+                region_code=region_code,
+                credit_amount=credit_amount,
+            )
+            if status == "SUCCESS":
+                user_credit = CreditWallet.objects.get(user=user)
+                user_credit.balance += credit_amount
+                user_credit.save()
+            return Response({'message': 'Transaction processed successfully'}, status=status.HTTP_200_OK)
         
 
     except Exception as e:
